8c149068b312bf3eccb123452708d4ea4c31c71f
Venisha08 <venishan2003@gmail.com>
2026-09-16 22:55:33 +0530

    sum of array

---
+++ sumOfArray.java
@@ -0,0 +1,27 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int array[] = new int[5];
+        System.out.println("Enter 5 number:");
+
+        for(int i =0 ; i< array.length; i++)
+            {
+                array[i] = sc.nextInt();                
+            }
+        int sum = 0;
+        System.out.println("Array elements:");
+           for(int i =0 ; i< array.length; i++)
+            {
+                System.out.println(array[i]);
+                sum += array[i];
+            }
+                        System.out.println("sum:"+sum);
+
+        
+    }
+}
