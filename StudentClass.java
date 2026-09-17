1d35ca6aa81bc53bddc0b3226e4dfb1d3b98d99c
Venisha08 <venishan2003@gmail.com>
2026-09-17 13:50:53 +0530

    Created object and displayed

---
+++ StudentClass.java
@@ -0,0 +1,31 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+
+class Student{
+
+    String name ;
+    int age;
+    int mark;
+
+    Student(String name,int age,int mark)
+    {
+        this.name = name;
+        this.age = age;
+        this.mark = mark;
+    }
+    
+    
+   void display_details()
+    {
+        System.out.println("Name:"+ name+" "+"Age:"+" "+ age+" " + "Mark:"+mark);
+    }
+}
+class Main {
+    public static void main(String[] args) {
+
+        Student sdt = new Student("venisha",23,85);
+        
+        sdt.display_details();
+    }
+}
