a29358e928a5049fab26e294855cee3a0e6a2d25
Venisha08 <venishan2003@gmail.com>
2026-09-20 21:42:20 +0530

    create a dog object and call all three methods

---
+++ Animal_dog_Inheritance.java
@@ -0,0 +1,36 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+
+
+class Animal{
+    
+void eat()
+    {
+        System.out.println("Eat");
+        
+    }
+    void sleep()
+    {
+        System.out.println("sleep");
+    }
+}
+
+class Dog extends Animal{
+
+    void bark()
+    {
+        System.out.println("Barking dog");
+    }
+}
+class Main {
+    public static void main(String[] args) {
+
+     Dog d = new Dog();
+       
+        d.bark();
+         d.eat();
+        d.sleep();
+        
+    }
+}
